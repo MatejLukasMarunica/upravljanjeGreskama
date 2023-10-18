c96fffee11f5e44a21bd937721fa2dbef4df9d0e
MatejLukasMarunica <145869520+MatejLukasMarunica@users.noreply.github.com>
2023-10-18 08:25:11 +0200

    Add files via upload

---
+++ upravljanjeGreskama/upravljanjeGreskama/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace upravljanjeGreskama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int godRod;
+            bool kontrola = false;
+            do
+            {
+                Console.WriteLine("Upiši svoju godinu rođenja:  ");
+                try
+                {
+                    godRod = Convert.ToInt16(Console.ReadLine());
+                    Console.WriteLine("Rođen si: " + godRod + ".godine");
+                    kontrola = true;
+                }
+                catch (Exception greska)
+                {
+                    Console.WriteLine(greska.Message);
+                    kontrola = false;
+                }
+            } while (kontrola == false);
+            Console.ReadLine();
+
+        }
+    }
+}
